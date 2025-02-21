5bac2dc10bf251cb6275deee5d8a795f8d0b5ff5
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-02-21 23:46:34 +0800

    Finished 339A Helpful Maths

--- Codeforces/A/helpful_maths/main.cpp
+++ Codeforces/A/helpful_maths/main.cpp
@@ -2,15 +2,43 @@
 using namespace std;
 
 int main() {
-    string string;
-    cin >> string;
+    string string_input;
+    cin >> string_input;
+
+    if (string_input.length() == 1) {
+        cout << string_input;
+        return 0;
+    }
 
     int one_counter = 0;
     int two_counter = 0;
     int three_counter = 0;
 
     int i;
-    for (i = 0; i < s.length(); i++) {
-        if (s[i] == )
+    for (i = 0; i < string_input.length(); i++) {
+        if (string_input[i] == '1') {
+            one_counter += 1;
+        } else if(string_input[i] == '2' ) {
+            two_counter += 1;
+        } else if (string_input[i] == '3') {
+            three_counter += 1;
+        }
     }
-}
+
+    string output;
+    i = 0;
+    for (i = 0; i < one_counter; i++) {
+        output += "1+";
+    }
+    i = 0;
+    for (i = 0; i < two_counter; i++) {
+        output += "2+";
+    }
+    i=0;
+    for (i = 0; i < three_counter; i++) {
+        output += "3+";
+    }
+    output.erase(output.begin() + output.length()-1);
+
+    cout << output;
+} 
