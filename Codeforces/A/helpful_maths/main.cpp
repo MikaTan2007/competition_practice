9157d56e8f230374610b6b7cb99e8dd49741ef7f
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-02-21 14:29:51 +0800

    Started helpful_maths 339A

---
+++ Codeforces/A/helpful_maths/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string string;
+    cin >> string;
+
+    int one_counter = 0;
+    int two_counter = 0;
+    int three_counter = 0;
+
+    int i;
+    for (i = 0; i < s.length(); i++) {
+        if (s[i] == )
+    }
+}
